9ac789de3a16d4752fe6bb6b890b74c2c6293efc
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-06-19 14:18:08 -0700

    refactored express server

--- server/express.js
+++ server/express.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
 const app = express();
-const port = 3003;
+const port = 3004;
+app.use(express.json());
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.get('/api/pledges/:id', (req, res) => {
+  console.log('req.params: ', req.params);
+  res.send('/api/pledges endpoint firing');
+});
+
+
+
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
