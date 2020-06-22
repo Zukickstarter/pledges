e008a3b6e838aea3843e6f4d1863a4f89cbfc2b2
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-06-22 14:28:09 -0700

    App component makes get request when successfully mounted: - id '6' hard coded into url for now - sets this.state.pledges to response.data from the server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      pledges: []
     }
   }
 
+  componentDidMount() {
+    const { pledges } = this.state;
+    console.log('component did mount');
+    axios.get('http://localhost:3003/api/pledges/6')
+    .then((response) => {
+      this.setState({ pledges: response.data });
+    })
+    .catch((err) => {
+      console.log('error with get request: ', err);
+    });
+  };
+
   render() {
-    const {  } = this.state;
+    const { pledges } = this.state;
     return (
         <div className="AppComponentDiv">
           THIS IS THE APP COMPONENT
         </div>
-
     );
   }
 };
