47b45a5e672c918392bccbfb68b3f9a45ebea342
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-06-24 13:31:47 -0700

    all data for pledges component rendered to page.. - modal not included

--- src/components/SinglePledge.jsx
+++ src/components/SinglePledge.jsx
@@ -11,14 +11,37 @@ const SinglePledge = (props) => {
   const { id, price, pledgeTitle, description, estDelivery, backers, listingId } = pledge;
   return (
   <div className="pledgeBox">
-    <div>
-      ID: {id}
+    <div className="pledgePriceDiv">
+      Pledge {price} or more.
     </div>
-    <div>
-      PRICE: {price}
+    <div className="pledgeTitleDiv">
+      {pledgeTitle}
     </div>
-    <div>
-      TITLE: {pledgeTitle}
+    <div className="pledgeDescriptionDiv">
+      {description}
+    </div>
+    <div className="pledgeEstDeliveryDiv">
+      estimated delivery: {estDelivery}
+    </div>
+    <div className="pledgeBackersDiv">
+      backers: {backers}
+    </div>
+    <div className="pledgeDropDownDiv">
+      <div className="dropDownShippingDestDiv">
+        Shipping Destination:
+        <div>
+          <input type="text"></input>
+        </div>
+      </div>
+      <div className="dropDownPledgeAmountDiv">
+        Pledge Amount
+        <div>
+          <input type="text"></input>
+        </div>
+      </div>
+      <div className="dropDownContinueButton">
+        <button>Continue</button>
+      </div>
     </div>
   </div>
   );
